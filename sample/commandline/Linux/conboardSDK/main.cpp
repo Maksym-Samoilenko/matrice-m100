f2193f86f659cdb7bade1f683f960cd489ae028c
Maksym Samoilenko <maxsam125@gmail.com>
2016-03-30 10:08:39 +0000

    Actual changes

--- sample/commandline/Linux/conboardSDK/main.cpp
+++ sample/commandline/Linux/conboardSDK/main.cpp
@@ -12,37 +12,37 @@ using namespace WPP;
 #define make_vers(a, b, c, d)                                                               \
     (((a << 24) & 0xff000000) | ((b << 16) & 0x00ff0000) | ((c << 8) & 0x0000ff00) |           \
      (d & 0x000000ff))
-
-HardDriverManifold driver("/dev/ttyUSB0", 230400);
-driver.init();
+HardDriverManifold driver("/dev/ttyAMA0", 230400);
 CoreAPI api(&driver);
 ConboardSDKScript script(&api);
-APIThread *send = new APIThread(api, 1, port);
-APIThread *read = new APIThread(api, 2, port);
-send->start();
-read->start();
+
+
 
 
 
 const int version = (make_vers(3, 1, 10, 0));
 
 const char * get_index_html(){
-    return "  <!DOCTYPE html>\n" +
+}
+
+void index(Request* req, Response* res) {
+    //globalDJIScript.addTask("CA");
+    //globalDJIScript.run();
+    res->body << std::string("  <!DOCTYPE html>\n") +
 				"  <html>\n" +
 				"  <head>\n" +
 				"      <script src=\"http://code.jquery.com/jquery-latest.min.js\"\n" +
 				"              type=\"text/javascript\"></script>\n" +
 				"\n" +
 				"      <script type=\"text/javascript\">\n" +
-				"          console.log('init');\n" +
+				"          console.log(\"init\");\n" +
 				"\n" +
 				"          function takeoff(){\n" +
 				"              console.log(\"takeoff\");\n" +
 				"              $.ajax({\n" +
 				"                  url: \"takeoff\",\n" +
 				"                  success: function( data ) {\n" +
-				"                    console.log(data);\n" +
-				"                      console.log('takeoff ok')\n" +
+				"                      console.log(\"takeoff ok\")\n" +
 				"                  }\n" +
 				"              });\n" +
 				"          }\n" +
@@ -50,10 +50,9 @@ const char * get_index_html(){
 				"          function release(){\n" +
 				"              console.log(\"release\");\n" +
 				"              $.ajax({\n" +
-				"                  url: \"/release\",\n" +
+				"                  url: \"/release_control\",\n" +
 				"                  success: function( data ) {\n" +
-				"                    console.log(data);\n" +
-				"                      console.log('release ok')\n" +
+				"                      console.log(\"release ok\")\n" +
 				"                  }\n" +
 				"              });\n" +
 				"          }\n" +
@@ -61,10 +60,9 @@ const char * get_index_html(){
 				"          function obtain(){\n" +
 				"              console.log(\"obtain control\");\n" +
 				"              $.ajax({\n" +
-				"                  url: \"/obtain\",\n" +
+				"                  url: \"/obtain_control\",\n" +
 				"                  success: function( data ) {\n" +
-				"                    console.log(data);\n" +
-				"                      console.log('obtain control ok')\n" +
+				"                      console.log(\"obtain control ok\")\n" +
 				"                  }\n" +
 				"              });\n" +
 				"          }\n" +
@@ -74,8 +72,7 @@ const char * get_index_html(){
 				"              $.ajax({\n" +
 				"                  url: \"/landing\",\n" +
 				"                  success: function( data ) {\n" +
-				"                    console.log(data);\n" +
-				"                      console.log('landing ok')\n" +
+				"                      console.log(\"landing ok\")\n" +
 				"                  }\n" +
 				"              });\n" +
 				"          }\n" +
@@ -85,8 +82,7 @@ const char * get_index_html(){
 				"              $.ajax({\n" +
 				"                  url: \"/activate\",\n" +
 				"                  success: function( data ) {\n" +
-				"                    console.log(data);\n" +
-				"                      console.log('activate ok')\n" +
+				"                      console.log(\"activate ok\")\n" +
 				"                  }\n" +
 				"              });\n" +
 				"          }\n" +
@@ -96,8 +92,7 @@ const char * get_index_html(){
 				"              $.ajax({\n" +
 				"                  url: \"/telemetry_data_time\",\n" +
 				"                  success: function( data ) {\n" +
-				"                    alert.log(data);\n" +
-				"                      console.log('telemetry_data_status ok')\n" +
+				"                      alert(data);\n" +
 				"                  }\n" +
 				"              });\n" +
 				"          }\n" +
@@ -107,8 +102,7 @@ const char * get_index_html(){
 				"              $.ajax({\n" +
 				"                  url: \"/telemetry_data_status\",\n" +
 				"                  success: function( data ) {\n" +
-				"                    alert.log(data);\n" +
-				"                      console.log('telemetry_data_status ok')\n" +
+				"                      alert(data);\n" +
 				"                  }\n" +
 				"              });\n" +
 				"          }\n" +
@@ -118,8 +112,7 @@ const char * get_index_html(){
 				"              $.ajax({\n" +
 				"                  url: \"/telemetry_data_battery\",\n" +
 				"                  success: function( data ) {\n" +
-				"                       alert.log(data);\n" +
-				"                      console.log('telemetry_data_battery ok')\n" +
+				"                      alert(data);\n" +
 				"                  }\n" +
 				"              });\n" +
 				"          }\n" +
@@ -129,67 +122,104 @@ const char * get_index_html(){
 				"<button onclick=\"activate()\">ACTIVATE</button>\n" +
 				"<button onclick=\"obtain()\">OBTAIN CONTROL</button>\n" +
 				"<button onclick=\"release()\">RELEASE CONTROL</button>\n" +
-				"<button onclick=\"takeoff()\">START</button>\n" +
-				"<button onclick=\"telemetry_data_time()\">TIME</button>\n" +
-				"<button onclick=\"telemetry_data_status()\">STATUS</button>\n" +
-				"<button onclick=\"telemetry_data_battery()\">BATTERY</button>\n" +
-				"</body>\n" +
-				"</html>";
-}
+				"  <button onclick=\"takeoff()\">START</button>\n" + "<button onclick=\"landing()\">LANDING</button>\n" +
 
-void index(Request* req, Response* res) {
-    //globalDJIScript.addTask("CA");
-    //globalDJIScript.run();
-    res->body << get_index_html();
+				"  <button onclick=\"telemetry_data_time()\">TIME</button>\n" +
+				"  <button onclick=\"telemetry_data_status()\">STATUS</button>\n" +
+				"  <button onclick=\"telemetry_data_battery()\">BATTERY</button>\n" +
+				"  </body>\n" +
+				"  </html>\n";
 }
 void takeoff(Request* req, Response* res) {
     script.getFlight()->task((DJI::onboardSDK::Flight::TASK)4);
-    res->body << "takeoff";
+
+sleep(5);
+cout << "takeoff" << endl;    
+res->body << "takeoff";
 }
 void landing(Request* req, Response* res) {
+cout << "landing function" << endl;
     script.getFlight()->task((DJI::onboardSDK::Flight::TASK)6);
-    res->body << "landing";
+
+sleep(5);
+cout << "landing"  << endl;
+   res->body << "landing";
 }
 void activate(Request* req, Response* res) {
-    script.adata.ID = 2;//ID
+    script.adata.ID = 1005002;//ID
     script.adata.reserved = 2;
     script.adata.version = version;
-    script.adata.encKey = "key";//KEy
-    script.getApi()->activate(&script.adata);
-    res->body << "activated";
+    script.adata.encKey = "390b75d49d51e9cea4f8724bfc4b1549eb2a325c3239d1e3b6ea3cccd37b7b9f";//KEy
+    
+script.getApi()->activate(&script.adata);
+sleep(4);
+cout << "activated" << endl;    
+res->body << "activated";
 }
 void release_control(Request* req, Response* res) {
-    script.getApi()->setControl(false);
-    res->body << "release_control";
+cout << "release control" << endl; 
+   script.getApi()->setControl(false);
+cout << "release control" << endl;
+sleep(4);   
+ res->body << "release_control";
 }
 void obtain_control(Request* req, Response* res) {
-    script.getApi()->setControl(true);
-    res->body << "obtain_control";
+cout << "obtain control function" << endl; 
+   script.getApi()->setControl(true);
+
+sleep(4);
+cout << "control obtained" << endl; 
+   res->body << "obtain_control";
 }
 
 void telemetry_data_time(Request* req, Response* res) {
     //Telemetry
-    BroadcastData bd = script.getApi()->getBroadcastData();
-    res->body << bd.timeStamp.time;
+    BroadcastData bd =(&script)->getApi()->getBroadcastData();
+sleep(3);
+cout << "time" << endl; 
+
+   res->body << bd.timeStamp.time;
 }
 
 void telemetry_data_status(Request* req, Response* res) {
     //Telemetry
-    BroadcastData bd = script.getApi()->getBroadcastData();
+    //Telemetry
+    //Telemetry
 
-    res->body << (int)bd.status;
+script.getApi()->setSyncFreq(2);
+    BroadcastData bd = script.getApi()->getBroadcastData();
+sleep(3);
+cout << "status" << endl;
+    res->body << bd.status;
 }
 
 void telemetry_data_battery(Request* req, Response* res) {
     //Telemetry
     BroadcastData bd = script.getApi()->getBroadcastData();
-
-    res->body << (int)bd.battery;
+sleep(2);
+cout <<"battery" << endl;    
+sleep(2);
+cout << (int)bd.battery << endl;
+sleep(2);
+cout << (int)(&script)->getApi()->getBatteryCapacity() << endl;
+sleep(2);
+cout << script.getApi()->getBatteryCapacity() << endl;
+sleep(2);
+bd = (&script)->getApi()->getBroadcastData();
+sleep(2);
+cout << (float)bd.battery << endl;
+cout << bd.battery << endl;
+ res->body << (float)bd.battery;
 }
 
 
-int main(int argc, const char* argv[]) {
 
+int main(int argc, const char* argv[]) {
+driver.init();
+APIThread send(&api, 1);
+APIThread read(&api, 2);
+send.createThread();
+read.createThread();
     try {
         std::cout << "Listening on port 5000" << std::endl;
         WPP::Server server;
