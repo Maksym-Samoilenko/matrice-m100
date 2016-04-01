d66c80673aefbc426d2125f3dab5c267bb6a64c2
Max <max@localhost.localdomain>
2016-04-01 08:26:50 +0300

    Actual changes

--- sample/commandline/Linux/conboardSDK/main.cpp
+++ sample/commandline/Linux/conboardSDK/main.cpp
@@ -8,6 +8,7 @@
 #include "web++.hpp"
 #include "cmdIO.h"
 #include "cmdFollow.h"
+#include <string>
 using namespace std;
 using namespace WPP;
 using namespace DJI::onboardSDK;
@@ -17,10 +18,6 @@ using namespace DJI::onboardSDK;
 HardDriverManifold driver("/dev/ttyUSB0", 230400);
 CoreAPI api(&driver);
 ConboardSDKScript script(&api);
-
-
-
-
 
 const int version = (make_vers(3, 1, 10, 0));
 
@@ -121,7 +118,7 @@ void index(Request* req, Response* res) {
                 "          function follow_point(){\n" +
                 "              console.log(\"follow_point\");\n" +
                 "              $.ajax({\n" +
-                "                  url: \"/follow_point\",\n" +
+                "                  url: \"/follow_point?x=\"  + $(\"#x_selector\").val() + \"&y=\" + $(\"#y_selector\").val() +\"&z=\" + $(\"#z_selector\").val() +\"&yaw=\" + $(\"#yaw_selector\").val()" + ",\n"
                 "                  success: function( data ) {\n" +
                 "                      console.log(data);\n" +
                 "                  }\n" +
@@ -137,113 +134,109 @@ void index(Request* req, Response* res) {
 
                 "  <button onclick=\"telemetry_data_time()\">TIME</button>\n" +
                 "  <button onclick=\"telemetry_data_status()\">STATUS</button>\n" +
-                "  <button onclick=\"telemetry_data_battery()\">BATTERY</button>\n" +
-                "  <button onclick=\"follow_point()\">FOLLOW</button>\n" +
+                "  <button onclick=\"telemetry_data_battery()\">BATTERY</button><br>\n" +
+
+          "<p>X</p><select name=\"x_sel\" id=\"x_selector\"><option value=\"0\">0</option><option value=\"0.25\">0.25</option><option value=\"0.5\">0.5</option><option value=\"0.75\">0.75</option><option value=\"1\">1</option><option value=\"1.25\">1.25</option><option value=\"1.5\">1.5</option><option value=\"1.75\">1.75</option><option value=\"2\">2</option><option value=\"-0.25\">-0.25</option><option value=\"-0.5\">-0.5</option><option value=\"-0.75\">-0.75</option><option value=\"-1\">-1</option><option value=\"-1.25\">-1.25</option><option value=\"-1.5\">-1.5</option><option value=\"-1.75\">-1.75</option><option value=\"-2\">-2</option></select>\n"+
+          "<p>Y</p><select name=\"y_sel\" id=\"y_selector\"><option value=\"0\">0</option><option value=\"0.25\">0.25</option><option value=\"0.5\">0.5</option><option value=\"0.75\">0.75</option><option value=\"1\">1</option><option value=\"1.25\">1.25</option><option value=\"1.5\">1.5</option><option value=\"1.75\">1.75</option><option value=\"2\">2</option><option value=\"-0.25\">-0.25</option><option value=\"-0.5\">-0.5</option><option value=\"-0.75\">-0.75</option><option value=\"-1\">-1</option><option value=\"-1.25\">-1.25</option><option value=\"-1.5\">-1.5</option><option value=\"-1.75\">-1.75</option><option value=\"-2\">-2</option></select>\n"+
+          "<p>Z</p><select name=\"z_sel\" id=\"z_selector\"><option value=\"0\">0</option><option value=\"0.25\">0.25</option><option value=\"0.5\">0.5</option><option value=\"0.75\">0.75</option><option value=\"1\">1</option><option value=\"1.25\">1.25</option><option value=\"1.5\">1.5</option><option value=\"1.75\">1.75</option><option value=\"2\">2</option><option value=\"-0.25\">-0.25</option><option value=\"-0.5\">-0.5</option><option value=\"-0.75\">-0.75</option><option value=\"-1\">-1</option><option value=\"-1.25\">-1.25</option><option value=\"-1.5\">-1.5</option><option value=\"-1.75\">-1.75</option><option value=\"-2\">-2</option></select>\n"+
+          "<p>YAW</p><select name=\"yaw_sel\" id=\"yaw_selector\"><option value=\"0\">0</option><option value=\"0.25\">0.25</option><option value=\"0.5\">0.5</option><option value=\"0.75\">0.75</option><option value=\"1\">1</option><option value=\"1.25\">1.25</option><option value=\"1.5\">1.5</option><option value=\"1.75\">1.75</option><option value=\"2\">2</option><option value=\"-0.25\">-0.25</option><option value=\"-0.5\">-0.5</option><option value=\"-0.75\">-0.75</option><option value=\"-1\">-1</option><option value=\"-1.25\">-1.25</option><option value=\"-1.5\">-1.5</option><option value=\"-1.75\">-1.75</option><option value=\"-2\">-2</option></select>\n"+
+          "  <button onclick=\"follow_point()\">FOLLOW</button>\n" +
                 "  </body>\n" +
                 "  </html>\n";
 }
 void takeoff(Request* req, Response* res) {
-    script.getFlight()->task((DJI::onboardSDK::Flight::TASK)4);
-
-sleep(5);
-cout << "takeoff" << endl;    
-res->body << "takeoff";
+  script.getFlight()->task((DJI::onboardSDK::Flight::TASK)4);
+  sleep(3);
+  cout << "takeoff" << endl;
+  res->body << "takeoff";
 }
 void landing(Request* req, Response* res) {
-cout << "landing function" << endl;
-    script.getFlight()->task((DJI::onboardSDK::Flight::TASK)6);
-
-sleep(5);
-cout << "landing"  << endl;
-   res->body << "landing";
+  script.getFlight()->task((DJI::onboardSDK::Flight::TASK)6);
+  sleep(3);
+  cout << "landing"  << endl;
+  res->body << "landing";
 }
 void activate(Request* req, Response* res) {
-    script.adata.ID = 1005002;//ID
-    script.adata.reserved = 2;
-    script.adata.version = version;
-    script.adata.encKey = "390b75d49d51e9cea4f8724bfc4b1549eb2a325c3239d1e3b6ea3cccd37b7b9f";//KEy
-    
-script.getApi()->activate(&script.adata);
-sleep(4);
-cout << "activated" << endl;    
-res->body << "activated";
+  script.adata.ID = 1005002;//ID
+  script.adata.reserved = 2;
+  script.adata.version = version;
+  script.adata.encKey = "390b75d49d51e9cea4f8724bfc4b1549eb2a325c3239d1e3b6ea3cccd37b7b9f";//KEy
+
+  script.getApi()->activate(&script.adata);
+  sleep(3);
+  cout << "activated" << endl;
+  res->body << "activated";
 }
 void release_control(Request* req, Response* res) {
-cout << "release control" << endl; 
-   script.getApi()->setControl(false);
-cout << "release control" << endl;
-sleep(4);   
- res->body << "release_control";
+  script.getApi()->setControl(false);
+  cout << "release control" << endl;
+  sleep(3);
+  res->body << "release_control";
 }
 void obtain_control(Request* req, Response* res) {
-cout << "obtain control function" << endl; 
-   script.getApi()->setControl(true);
-
-sleep(4);
-cout << "control obtained" << endl; 
-   res->body << "obtain_control";
+  cout << "obtain control function" << endl;
+  script.getApi()->setControl(true);
+  sleep(3);
+  cout << "control obtained" << endl;
+  res->body << "obtain_control";
 }
 
 void telemetry_data_time(Request* req, Response* res) {
-    //Telemetry
-    BroadcastData bd =(&script)->getApi()->getBroadcastData();
-sleep(3);
-cout << "time" << endl; 
-
-   res->body << bd.timeStamp.time;
+  BroadcastData bd =(&script)->getApi()->getBroadcastData();
+  sleep(3);
+  cout << "time" << endl;
+  res->body << bd.timeStamp.time;
 }
 
 void telemetry_data_status(Request* req, Response* res) {
-
-script.getApi()->setSyncFreq(1);
-    BroadcastData bd = script.getApi()->getBroadcastData();
-sleep(3);
-cout << "status" << endl;
-    res->body << bd.status;
+  script.getApi()->setSyncFreq(1);
+  BroadcastData bd = script.getApi()->getBroadcastData();
+  sleep(3);
+  cout << "status" << endl;
+  res->body << bd.status;
 }
 
 void telemetry_data_battery(Request* req, Response* res) {
-    //Telemetry
-    BroadcastData bd = script.getApi()->getBroadcastData();
-sleep(2);
-cout <<"battery" << endl;    
-sleep(2);
-cout << (int)bd.battery << endl;
-sleep(2);
-cout << (int)(&script)->getApi()->getBatteryCapacity() << endl;
-sleep(2);
-cout << script.getApi()->getBatteryCapacity() << endl;
-sleep(2);
-bd = (&script)->getApi()->getBroadcastData();
-sleep(2);
-cout << (float)bd.battery << endl;
-cout << bd.battery << endl;
- res->body << (float)bd.battery;
+  BroadcastData bd = script.getApi()->getBroadcastData();
+  sleep(1);
+  res->body << (float)bd.battery;
 }
-float coordx = 11;
-float coordy =11;
-float coordz = 11;
-float coordyaw = 11;
+
 void follow_point(Request* req, Response* res) {
 FlightData data;
-
 data.flag = 0x90;
-data.x = coordx;
-data.y = coordy;
-data.z = coordz;
-data.yaw = coordyaw;
-script.getFlight()->setFlight(&data);
-sleep(1);
- res->body << "follow mission";
+std::map<std::string, std::string>::iterator iter;
+for (iter = req->query.begin(); iter != req->query.end(); ++iter) {
+std::string val = iter->first;
+  if(val.compare("x") == 0){
+    data.x = stod(iter->second);
+    std::cout << iter->first<< " = " << iter->second  << "  " << data.x << std::endl;
+  }
+  else if(val.compare("y") == 0){
+    data.y = stod(iter->second);
+    std::cout << iter->first<< " = " << iter->second << "  " << data.y << std::endl;
+  }
+  else if(val.compare("z") == 0){
+    data.z = stod(iter->second);
+    std::cout << iter->first<< " = " << iter->second << "  " << data.z  << std::endl;
+  }
+  else if(val.compare("yaw") == 0){
+    data.yaw= stod(iter->second);
+    std::cout << iter->first<< " = " << iter->second  << "  " << data.yaw << std::endl;
+  }
+}
+  script.getFlight()->setFlight(&data);
+  sleep(1);
+  res->body << "follow mission";
 }
 
 
 int main(int argc, const char* argv[]) {
-driver.init();
-APIThread send(&api, 1);
-APIThread read(&api, 2);
-send.createThread();
-read.createThread();
+    driver.init();
+    APIThread send(&api, 1);
+    APIThread read(&api, 2);
+    send.createThread();
+    read.createThread();
     try {
         std::cout << "Listening on port 5000" << std::endl;
         WPP::Server server;
